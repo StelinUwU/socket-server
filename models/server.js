9ee93cf59784b8e62e25ab3c0b819ebbef7d4ef6
Stelin <stelinduarte100@gmail.com>
2021-09-19 00:04:49 -0600

    :recycle: Cleaning code

--- models/server.js
+++ models/server.js
@@ -4,7 +4,7 @@ const http = require("http");
 const socketio = require("socket.io");
 const path = require("path");
 const Sockets = require("./sockets");
-
+const cors = require("cors");
 class Server {
   constructor() {
     this.app = express();
@@ -20,7 +20,11 @@ class Server {
     });
   }
   middlewares() {
+    //Desplegar directorio publico
     this.app.use(express.static(path.resolve(__dirname, "../public")));
+
+    //CORS
+    this.app.use(cors());
   }
 
   configureSockets() {
