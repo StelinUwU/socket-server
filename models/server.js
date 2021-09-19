a10261dfb891f72d79b78689c10daa1c63a945b5
Stelin <stelinduarte100@gmail.com>
2021-09-18 23:43:29 -0600

    :sparkles: First commit

---
+++ models/server.js
@@ -0,0 +1,44 @@
+//Servidor de express
+const express = require("express");
+const http = require("http");
+const socketio = require("socket.io");
+const path = require("path");
+const Sockets = require("./sockets");
+
+class Server {
+  constructor() {
+    this.app = express();
+    this.port = process.env.PORT || 8080;
+
+    //Http server
+    this.server = http.createServer(this.app);
+
+    //Config de sockets
+    //Configuración del socket server
+    this.io = socketio(this.server, {
+      /* configuraciones */
+    });
+  }
+  middlewares() {
+    this.app.use(express.static(path.resolve(__dirname, "../public")));
+  }
+
+  configureSockets() {
+    new Sockets(this.io);
+  }
+
+  execute() {
+    //Inicializar middlewares
+    this.middlewares();
+
+    //Inicializar sockets
+    this.configureSockets();
+
+    //Inicializar server
+    this.server.listen(this.port, () => {
+      console.log("Server corriendo en puerto:", this.port);
+    });
+  }
+}
+
+module.exports = Server;
